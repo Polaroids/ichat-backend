4cc1073145fd61d94114fe7f8ab45ccc42acd1ce
Bluefly7 <1213112305@qq.com>
2020-06-10 11:09:40 +0800

    chat

--- src/main/java/org/buaa/ichat/service/MessageService.java
+++ src/main/java/org/buaa/ichat/service/MessageService.java
@@ -1,7 +1,22 @@
 package org.buaa.ichat.service;
 
+import org.buaa.ichat.entity.GroupMSG;
+import org.buaa.ichat.entity.Message;
+import org.json.JSONObject;
+
+import java.util.List;
+
 public interface MessageService {
     //单独聊天的消息
-    public void insertMSG(Integer userID,String content,Integer receiverID)throws Exception;
-    public void insertGMSG(Integer userID,String content,Integer groupID)throws Exception;
+    public Integer insertMSG(Integer userID,String content,Integer receiverID)throws Exception;
+    public Integer insertGMSG(Integer userID,String content,Integer groupID)throws Exception;
+    public Message getMSGByID(Integer messageID)throws Exception;
+    public List<Message> getNoSendMSG(Integer userID, Integer receiverID)throws Exception;
+    public GroupMSG getGMSGByID(Integer GM_ID)throws Exception;
+    public List<GroupMSG> getNoSendGMSG(Integer groupID, Integer receiverID)throws Exception;
+    public void updateNoSentMSG(Integer msgID)throws Exception;
+    public void updateNoSentGMSG(Integer gmsgID, Integer receiverID)throws Exception;
+    public List<Message> getHistoryMSG(Integer userID, Integer msgID)throws Exception;
+    public List<GroupMSG> getHistoryGMSG(Integer groupID, Integer gmsgID)throws Exception;
+    public List<JSONObject> getChatList()throws Exception;
 }
